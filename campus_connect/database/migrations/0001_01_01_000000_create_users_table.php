0c557e2817492ce7fc09d112d3f581d96267f59c
MagmusTop <jujupopobubu@gmail.com>
2025-11-12 16:22:57 +0100

    MàJ migrate

--- campus_connect/database/migrations/0001_01_01_000000_create_users_table.php
+++ campus_connect/database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->foreignId('role_id')->constrained('roles');
-            $table->string('fullname');
+            $table->string('nom');
+            $table->string('prenom');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable(); 
             $table->string('password');
